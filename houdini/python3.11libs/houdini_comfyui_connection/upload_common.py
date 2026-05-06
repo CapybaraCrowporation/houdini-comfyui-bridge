760927176eaf0bfde7af65484700185fc7f82b00
permahorse <13556996+permahorse@users.noreply.github.com>
2026-05-06 12:05:47 +0200

    upload function returns path as dictated by the server

--- houdini/python3.11libs/houdini_comfyui_connection/upload_common.py
+++ houdini/python3.11libs/houdini_comfyui_connection/upload_common.py
@@ -1,8 +1,9 @@
 from .requester import Requester
+from .logging import debug
 from pathlib import Path
 
 
-def upload_image(requester: Requester, file_path: Path, subdir: str, image_name: str|None):
+def upload_image(requester: Requester, file_path: Path, subdir: str, image_name: str|None) -> tuple[str, str]:
     if image_name is None:
         image_name = file_path.name
         
@@ -17,3 +18,6 @@ def upload_image(requester: Requester, file_path: Path, subdir: str, image_name:
 
     if resp.status_code != 200:
         raise RuntimeError(f'oh no, server said nono {resp.status_code}')
+    # reply may contain actual image+subdir  {'name': 'f54ee43cac68376a2e898caf6a31b0dd81da69a71139ea127920699d9bdc9156.png', 'subfolder': '', 'type': 'input'}
+    resp_data = resp.json()
+    return resp_data.get('name', image_name), resp_data.get('subfolder', subdir)
